f403dc74c4c8e4985140121657344ce576eceb90
Max <max@Maxs-MacBook-Air.local>
2020-05-04 17:08:16 -0400

    commented out fields not present in XML file

--- wp-opendoor-add-on.php
+++ wp-opendoor-add-on.php
@@ -23,10 +23,10 @@ $open_door_theme_addon->fields_text = array(
         'id' => 'pricecustom_value',
         'label' => 'Custom price text (Example: "As low as $200,000")',
     ],
-    (object) [
-        'id' => 'reducedby_value',
-        'label' => 'Price reduction (optional)'
-    ],
+    // (object) [
+    //     'id' => 'reducedby_value',
+    //     'label' => 'Price reduction (optional)'
+    // ],
     (object) [
         'id' => 'address_value',
         'label' => 'Address for the listing',
@@ -37,20 +37,20 @@ $open_door_theme_addon->fields_text = array(
         'label' => 'City and State/Province (or similar for your area)',
         'hint' => 'In one line. Note: This is for display, and Google Maps use only, NOT for use in the search.',
     ],
-    (object) [
-        'id' => 'mls_value',
-        'label' => 'MLS number (required only if \'mlslisting_value\' is Yes)',
-    ],
-    (object) [
-        'id' => 'openhousedate_value',
-        'label' => 'Open House Date (Required if this is searchable)',
-        'hint' => 'If this property has an upcoming Open House, enter the date. For example: \'July 12, 2013\'. The theme will automatically disable any reference to the open house when the date expires.',
-    ],
-    (object) [
-        'id' => 'openhousetime_value',
-        'label' => 'Open House Time',
-        'hint' => 'If the property has an upcoming Open House, enter the time range. For example: \'1-4pm\'',
-    ],
+    // (object) [
+    //     'id' => 'mls_value',
+    //     'label' => 'MLS number (required only if \'mlslisting_value\' is Yes)',
+    // ],
+    // (object) [
+    //     'id' => 'openhousedate_value',
+    //     'label' => 'Open House Date (Required if this is searchable)',
+    //     'hint' => 'If this property has an upcoming Open House, enter the date. For example: \'July 12, 2013\'. The theme will automatically disable any reference to the open house when the date expires.',
+    // ],
+    // (object) [
+    //     'id' => 'openhousetime_value',
+    //     'label' => 'Open House Time',
+    //     'hint' => 'If the property has an upcoming Open House, enter the time range. For example: \'1-4pm\'',
+    // ],
     (object) [
         'id' => 'beds_value',
         'label' => 'Bedrooms (Required if this is searchable)',
@@ -61,28 +61,28 @@ $open_door_theme_addon->fields_text = array(
         'label' => 'Bathrooms (Required if this is searchable)',
         'hint' => 'How many bathrooms does this property have? This is used when visitors search by number of bathrooms.',
     ],
-    (object) [
-        'id' => 'size_value',
-        'label' => 'Size (Required if this is searchable)',
-        'hint' => 'The home/building size. Only number, no commas or text. For example: 5500. This represents the number of square feet, acres, etc, as determined as your setting in Theme Options.',
-    ],
-    (object) [
-        'id' => 'lotsize_value',
-        'label' => 'Lot Size (Required if this is searchable)',
-        'hint' => 'The full lot size. Only a number, no comma or text. For example: 13500. This represents the number of square feet, acres, etc, as determined as your setting in Theme Options.',
-    ],
-    (object) [
-        'id' => 'garage_value',
-        'label' => 'Number of garage spaces (optional)'
-    ],
+    // (object) [
+    //     'id' => 'size_value',
+    //     'label' => 'Size (Required if this is searchable)',
+    //     'hint' => 'The home/building size. Only number, no commas or text. For example: 5500. This represents the number of square feet, acres, etc, as determined as your setting in Theme Options.',
+    // ],
+    // (object) [
+    //     'id' => 'lotsize_value',
+    //     'label' => 'Lot Size (Required if this is searchable)',
+    //     'hint' => 'The full lot size. Only a number, no comma or text. For example: 13500. This represents the number of square feet, acres, etc, as determined as your setting in Theme Options.',
+    // ],
+    // (object) [
+    //     'id' => 'garage_value',
+    //     'label' => 'Number of garage spaces (optional)'
+    // ],
     (object) [
         'id' => 'date_value',
         'label' => 'Date Available (format: July 12, 2013)'
     ],
-    (object) [
-        'id' => 'year_value',
-        'label' => 'Year built (e.g. 1980) (Required if this is searchable)'
-    ],
+    // (object) [
+    //     'id' => 'year_value',
+    //     'label' => 'Year built (e.g. 1980) (Required if this is searchable)'
+    // ],
     (object) [
         'id' => 'school_value',
         'label' => 'School district (optional)'
@@ -92,27 +92,27 @@ $open_door_theme_addon->fields_text = array(
         'label' => 'Google Maps Location (optional)',
         'hint' => 'By default the Google Map for each listing uses the address, city, state, and Zip, as entered above. If you enter an address here, the Google map will use this info instead of the info entered above.',
     ],
-    (object) [
-        'id' => 'title_value',
-        'label' => 'Custom Slideshow Title (optional)',
-        'hint' => 'By default the slideshow title will pull in the text from the post Title. You can override that text by entering custom text here.',
-    ],
-    (object) [
-        'id' => 'email_value',
-        'label' => 'Agent\'s email address (optional)'
-    ],
-    (object) [
-        'id' => 'phoneoffice_value',
-        'label' => 'Agent\'s office number (optional)'
-    ],
-    (object) [
-        'id' => 'phonemobile_value',
-        'label' => 'Agent\'s mobile number (optional)'
-    ],
-    (object) [
-        'id' => 'fax_value',
-        'label' => 'Agent\'s fax number (definitely optional)'
-    ]
+    // (object) [
+    //     'id' => 'title_value',
+    //     'label' => 'Custom Slideshow Title (optional)',
+    //     'hint' => 'By default the slideshow title will pull in the text from the post Title. You can override that text by entering custom text here.',
+    // ],
+    // (object) [
+    //     'id' => 'email_value',
+    //     'label' => 'Agent\'s email address (optional)'
+    // ],
+    // (object) [
+    //     'id' => 'phoneoffice_value',
+    //     'label' => 'Agent\'s office number (optional)'
+    // ],
+    // (object) [
+    //     'id' => 'phonemobile_value',
+    //     'label' => 'Agent\'s mobile number (optional)'
+    // ],
+    // (object) [
+    //     'id' => 'fax_value',
+    //     'label' => 'Agent\'s fax number (definitely optional)'
+    // ]
 );
 
 foreach ( $open_door_theme_addon->fields_text as $field ) {
